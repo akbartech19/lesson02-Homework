a3251ac326dd2ad060eb2c227033b2942d38a51d
Akbar_tech <akbaraminjonov2@gmail.com>
2026-07-06 09:03:24 +0500

    loop01.py

---
+++ loop01.py
@@ -0,0 +1,15 @@
+def powerA3(x):
+    return x ** 3
+
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+d = int(input("d = "))
+e = int(input("e = "))
+
+print("a ning 3-darajasi:", powerA3(a))
+print("b ning 3-darajasi:", powerA3(b))
+print("c ning 3-darajasi:", powerA3(c))
+print("d ning 3-darajasi:", powerA3(d))
+print("e ning 3-darajasi:", powerA3(e))
