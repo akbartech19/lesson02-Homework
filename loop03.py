6ffb24ca4a2211ede447269e65460873a6aa3b15
Akbar_tech <akbaraminjonov2@gmail.com>
2026-07-06 09:15:02 +0500

    loop03.py

---
+++ loop03.py
@@ -0,0 +1,10 @@
+def even(k):
+    return k % 2 == 0
+
+a = int(input("1-sonni kiriting: "))
+b = int(input("2-sonni kiriting: "))
+c = int(input("3-sonni kiriting: "))
+
+print(a, "->", even(a))
+print(b, "->", even(b))
+print(c, "->", even(c))
